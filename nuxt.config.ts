1bd4b89db097846a9b6ac8bbe50f1417255077ec
Amiri <husenova270@gmail.com>
2025-09-19 11:44:44 +0400

    fourth commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,6 +25,7 @@ export default defineNuxtConfig({
   },
   supabase: {
     url: 'https://srrzfwevndjzkqogrxrq.supabase.co',
-    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNycnpmd2V2bmRqemtxb2dyeHJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc5NDMwNjksImV4cCI6MjA3MzUxOTA2OX0.Lit-cjaON1zTLIlsWm39NA37GbAoj5Sy4wcV8Uclt2k'
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNycnpmd2V2bmRqemtxb2dyeHJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc5NDMwNjksImV4cCI6MjA3MzUxOTA2OX0.Lit-cjaON1zTLIlsWm39NA37GbAoj5Sy4wcV8Uclt2k',
+    redirect: false
   }
 })
